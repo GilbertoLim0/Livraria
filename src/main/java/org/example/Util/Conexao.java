7a4c084865779078f525bc0563064fb42facec83
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-03-17 12:54:04 -0300

    Util create

---
+++ src/main/java/org/example/Util/Conexao.java
@@ -0,0 +1,15 @@
+package org.example.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:6543/postgres";
+    private static final String PASSWORD = "Gilbert0Lim!2605";
+    private static final String USER = "postgres.tcnkzmuvynehmvgeqhpf";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
