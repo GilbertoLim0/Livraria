5e9f79fa21d9312aa2fb9f16ad6fde0340f1c609
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-03-17 13:15:29 -0300

    BookDao contracts implementation

--- src/main/java/org/example/Dao/BookDao.java
+++ src/main/java/org/example/Dao/BookDao.java
@@ -1,4 +1,29 @@
 package org.example.Dao;
 
-public class BookDao {
+import org.example.Contract.BookContract;
+import org.example.Model.Book;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BookDao implements BookContract {
+    @Override
+    public Boolean createBook(Book book) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Book> readBook() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public Book updateBook(Book book) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteBook(Book book) throws SQLException {
+        return null;
+    }
 }
