5d5e44707217483cbc44ae5c6a3ba9ad9def5de6
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-03-17 13:16:28 -0300

    CategoryDao contracts implementation

--- src/main/java/org/example/Dao/CategoryDao.java
+++ src/main/java/org/example/Dao/CategoryDao.java
@@ -1,4 +1,29 @@
 package org.example.Dao;
 
-public class CategoryDao {
+import org.example.Contract.CategoryContract;
+import org.example.Model.Category;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class CategoryDao implements CategoryContract {
+    @Override
+    public Boolean createCategory(Category category) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Category> readCategory() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public Category updateCategory() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteCategory() throws SQLException {
+        return null;
+    }
 }
