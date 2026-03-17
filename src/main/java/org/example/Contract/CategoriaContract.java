95dbf17c5e07206f543ac9fdebb789bffebf74db
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-03-17 13:03:43 -0300

    Book contracts

--- src/main/java/org/example/Contract/CategoriaContract.java
+++ src/main/java/org/example/Contract/CategoriaContract.java
@@ -1,4 +1,5 @@
 package org.example.Contract;
 
 public interface CategoriaContract {
+
 }
--- src/main/java/org/example/Contract/LivroContract.java
+++ src/main/java/org/example/Contract/LivroContract.java
@@ -1,4 +1,13 @@
 package org.example.Contract;
 
+import org.example.Model.Livro;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface LivroContract {
+    public Boolean createBook(Livro livro) throws SQLException;
+    public List<Livro> readBook() throws SQLException;
+    public Livro updateBook(Livro livro) throws SQLException;
+    public Boolean deleteBook(Livro livro) throws SQLException;
 }
