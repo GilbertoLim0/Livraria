d711bafdb0ef2e24ec7aa8c0fa4f17dfa35fdbab
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-03-18 10:22:23 -0300

    BookService develop

--- src/main/java/org/example/Service/BookService.java
+++ src/main/java/org/example/Service/BookService.java
@@ -1,4 +1,26 @@
 package org.example.Service;
 
+import org.example.Contract.BookContract;
+import org.example.Contract.CategoryContract;
+import org.example.Model.Book;
+import org.example.Model.Category;
+
+import java.sql.SQLException;
+
 public class BookService {
+    BookContract bk;
+    CategoryContract cc;
+    public void triggerCreateBook(Book book) throws SQLException {
+        bk.createBook(book);
+    }
+    public void triggerReadBook(Category categoryIdentifier) throws SQLException {
+        bk.readBook(categoryIdentifier);
+    }
+    public void triggerUpdateBook(Book book) throws SQLException {
+        bk.updateBook(book);
+    }
+    public void triggerDeleteBook(Book book) throws SQLException {
+        bk.deleteBook(book);
+    }
+
 }
